842fadeb04edf2c8417139b1dae65d27b96fc57b
Dranhoc <christophe.h.leroy@gmail.com>
2024-04-25 09:43:11 +0200

    sedfse

--- components/App.jsx
+++ components/App.jsx
@@ -1,5 +1,24 @@
 import { useState } from 'react'
+const myKey = "13a14486a9a544b2b8adbff1d336d9ad";
 
+function loadAPI(){
+  fetch("https://api.rawg.io/api/games")
+  .then(response => response.json())
+  .then(data => {
+  
+    for (const key in data) {
+    
+        // const option = document.createElement('option');
+        // option.value = data;
+        // option.text = data[key];
+        console.log(data)
+
+        // selectElement.appendChild(option);
+    }
+  })
+  .catch(error => console.error('Erreur lors de la lecture du fichier JSON:', error));
+
+}
 function App() {
 
   return <div><h1>SALUT AURELIANE COMMENT CA VA ?</h1></div>
