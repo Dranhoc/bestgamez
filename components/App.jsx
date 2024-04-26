05b4b3d2546fdfc6295124eb47ef0b8e128d9755
Dranhoc <christophe.h.leroy@gmail.com>
2024-04-26 15:38:03 +0200

    Card creation

--- components/App.jsx
+++ components/App.jsx
@@ -1,18 +1,17 @@
-import React, {useEffect} from 'react';
+// import React, {useEffect} from 'react';
 import Api from './Api.jsx';
 import Header from './Header.jsx';
-import MainContent from './MainContent.jsx';
-import axios from "axios";
-import Card from './Card.jsx'
+// import MainContent from './MainContent.jsx';
+// import Card from './Card.jsx'
 
 
-// Api()
+
 
 function App() {
 
   return <>
     <Header />
-    <MainContent/>
+    <Api/>
  
   </>
   
