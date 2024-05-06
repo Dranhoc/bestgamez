06df880d8b76d97151db0b75cb1c9339bc2b05b6
Lameuhton <aurelianeg2000@gmail.com>
2024-05-06 11:10:35 +0200

    improved burger menu

--- components/NavMobile.jsx
+++ components/NavMobile.jsx
@@ -2,16 +2,36 @@ import { useClickAway } from "react-use";
 import { useRef } from "react";
 import { useState } from "react";
 import { Squash as Hamburger } from "hamburger-react";
-// import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 function NavMobile() {
-  const [isOpen, setOpen] = useState(false);
+    const [isOpen, setOpen] = useState(false);
+    const ref = useRef(null);
 
-  return (
-    <div className="navMobile">
-      <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
-    </div>
-  );
+    useClickAway(ref, () => setOpen(false));
+
+    return (
+        <div ref={ref} className="navMobile">
+        <Hamburger toggled={isOpen} size={25} toggle={setOpen} />
+        {isOpen && (
+            <div className="custom-navbar">
+                <ul>
+                    <li>
+                        <a onClick={() => setOpen((prev) => !prev)} href="#">Login</a>
+                    </li>
+
+                    <li>
+                        <a onClick={() => setOpen((prev) => !prev)} href="#">Sign Up</a>
+                    </li>
+
+                    <li>
+                        <a onClick={() => setOpen((prev) => !prev)} href="#">API</a>
+                    </li> 
+                </ul>
+          </div>
+        )}
+        </div>
+    );
 };
 
 export default NavMobile;
